1fd533b19824dfe69c079d5cf46e50488991e5b7
ustc.hj@gmail.com <ustc.hj@gmail.com>
2025-02-19 15:50:49 +0800

    Test loading a file with quickjs' std module.

--- tests/qjs/js/main.js
+++ tests/qjs/js/main.js
@@ -1,9 +1,16 @@
-import { MyClass } from "./lib.js";
-
-// JSValue myClass = JS_GetPropertyStr(ctx, global_obj, "MyClass");
-globalThis.MyClass = MyClass; // necessary to get the constructor in c++
-
 // test in commandline: `./qjs ./main.js`
-const obj = new MyClass(10);
-console.log(obj.greet("QuickJS"));
-console.log(obj.myMethod());
+import * as std from 'qjs:std'
+import { MyClass } from './lib.js'
+
+globalThis.MyClass = MyClass // necessary to get the constructor in c++
+
+const obj = new MyClass(10)
+console.log(obj.greet('QuickJS'))
+console.log(obj.myMethod())
+
+console.log(obj.greet?.name.includes('greet'))
+console.log(obj.hello?.name.includes('greet'))
+
+// test load file with utf-8 chars
+const content = std.loadFile('./lib.js')
+console.log('File contents => ', content)
