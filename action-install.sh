f99dbc1bcb395ecd34fc07e35085521e653b9487
ustc.hj@gmail.com <ustc.hj@gmail.com>
2025-03-22 09:24:22 +0800

    Add Windows CI build.

--- action-install.sh
+++ action-install.sh
@@ -3,7 +3,13 @@
 # This script installs the dependencies to build the project within libRime
 git submodule update --init --recursive
 
-brew install nodejs
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install nodejs
+elif [[ "$OSTYPE" == "linux-gnu"* ]] && [ -f /etc/lsb-release ]; then
+    sudo apt-get update
+    sudo apt-get install -y nodejs
+fi
+
 
 cd tests/js
 npm install
