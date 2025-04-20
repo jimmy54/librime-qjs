1efd2d08638cec37c73446163e56acfbf782cb16
ustc.hj@gmail.com <ustc.hj@gmail.com>
2025-04-20 10:53:26 +0800

    JavaScript Engine: log the performance data of running all filters.

--- src/gears/qjs_filter.hpp
+++ src/gears/qjs_filter.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <rime/context.h>
 #include <rime/engine.h>
 #include <rime/filter.h>
 #include <rime/gear/filter_commons.h>
+#include <chrono>
+#include <iomanip>
 #include <memory>
 
 #include "environment.h"
@@ -12,12 +15,29 @@
 
 template <typename T_JS_VALUE>
 class QuickJSFilter : public QjsModule<T_JS_VALUE> {
+  inline static std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds>
+      beginClock = std::chrono::steady_clock::now();
+
 public:
   explicit QuickJSFilter(const rime::Ticket& ticket, Environment* environment)
       : QjsModule<T_JS_VALUE>(ticket.name_space, environment, "filter") {}
 
   std::shared_ptr<rime::Translation> apply(std::shared_ptr<rime::Translation> translation,
                                            Environment* environment) {
+    if (this->getNamespace().find("benchmark_begin") != std::string::npos) {
+      beginClock = std::chrono::steady_clock::now();
+    } else if (this->getNamespace().find("benchmark_end") != std::string::npos) {
+      auto endClock = std::chrono::steady_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endClock - beginClock);
+      std::string engine = "jsc";
+      if constexpr (std::is_same_v<T_JS_VALUE, JSValue>) {
+        engine = "qjs";
+      }
+      LOG(INFO) << "[benchmark] all " << engine << " filters run for " << std::setw(3)
+                << duration.count()
+                << " ms, with input = " << environment->getEngine()->context()->input();
+    }
+
     if (!this->isLoaded()) {
       return translation;
     }
